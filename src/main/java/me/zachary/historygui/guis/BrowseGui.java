48d8ffb25df3d5ee918685ff87128621ad72ae79
Zachary Denis <2080094@etu.cegepsth.qc.ca>
2021-03-10 19:44:15 -0500

    Added ability to hide offline player in browser gui

--- src/main/java/me/zachary/historygui/guis/BrowseGui.java
+++ src/main/java/me/zachary/historygui/guis/BrowseGui.java
@@ -5,12 +5,16 @@ import me.zachary.historygui.Historygui;
 import me.zachary.historygui.utils.GuiUtils;
 import me.zachary.zachcore.guis.ZMenu;
 import me.zachary.zachcore.guis.buttons.ZButton;
+import me.zachary.zachcore.utils.ChatUtils;
 import me.zachary.zachcore.utils.items.ItemBuilder;
 import me.zachary.zachcore.utils.xseries.SkullUtils;
 import me.zachary.zachcore.utils.xseries.XMaterial;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -37,9 +41,10 @@ public class BrowseGui {
                 try (ResultSet rs = st.executeQuery()) {
                     while (rs.next()) {
                         OfflinePlayer target = Bukkit.getServer().getOfflinePlayer(UUID.fromString(rs.getString("uuid")));
-                        ZButton playerButton = new ZButton(new ItemBuilder(SkullUtils.getSkull(target.getUniqueId()))
-                                .name("&7" + target.getName())
-                                .build()).withListener(inventoryClickEvent -> {
+                        if(!plugin.getConfig().getBoolean("Offline player") && target.getPlayer() == null)
+                            continue;
+                        ZButton playerButton = new ZButton(getHeadItemDisplay(target))
+                                .withListener(inventoryClickEvent -> {
                             player.openInventory(new HistoryGui(plugin).getHistoryInventory(player, target));
                         });
 
@@ -62,5 +67,14 @@ public class BrowseGui {
                 e.printStackTrace();
             }
         });
+    }
+
+    public ItemStack getHeadItemDisplay(OfflinePlayer op) {
+        ItemStack item = new ItemStack(SkullUtils.getSkull(op.getUniqueId()).getType(), 1, (short) 3);
+        SkullMeta pSkull = (SkullMeta) item.getItemMeta();
+        pSkull.setDisplayName(ChatUtils.colorCode("&7" + op.getName()));
+        pSkull.setOwner(op.getName());
+        item.setItemMeta(pSkull);
+        return item;
     }
 }
